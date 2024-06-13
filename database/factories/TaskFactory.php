b0fd94dd3e7cc8dc312222b6861343decfd347ea
sanjeev <sanjeev@debian.sanjeev>
2024-06-14 00:28:03 +0530

    pushing

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -19,11 +19,17 @@ class TaskFactory extends Factory
         return [
         'name' => fake()->sentence(),
         'description'=> fake()->realText(),
-        'due_date'=> fake()->dateTimeBetween('now','+1 year'),
+        'due_date'=> fake()
+                    ->dateTimeBetween('now','+1 year'),
         'status'=> fake()->randomElement(['pending','in_progress','completed']),
+        'priority'=> fake()
+                    ->randomElement(['low','medium','high']),
         'image_path'=> fake()->imageUrl(),
-        'created_at'=> 1
-
+        'assigned_user_id' =>1,
+        'created_by'=> 1,
+        'updated_by'=> 1,
+        'created_at'=> time(),
+        'updated_at'=> time(),
         ];
     }
 }
---
+++ resources/js/Pages/Project/index.jsx
@@ -0,0 +1,10 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+
+export default function Index(){
+  return(
+<AuthenticatedLayout>
+  
+</AuthenticatedLayout>
+  )
+}
